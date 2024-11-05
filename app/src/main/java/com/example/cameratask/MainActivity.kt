4b0cb491aa44ec581a678e735904c1d3c9da802e
Alex Bahno <bahnoalex@gmail.com>
2024-11-05 21:50:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cameratask/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.cameratask
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cameratask.databinding.ActivityMainBinding
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageURL: Uri
+
+    // Intent to open Camera
+    private val contract = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+        binding.imageView.setImageURI(imageURL)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        imageURL = createImageUri()
+        binding.cameraButton.setOnClickListener {
+            openCamera()
+        }
+        binding.shareButton.setOnClickListener {
+            sharePicture()
+        }
+    }
+
+    private fun createImageUri(): Uri {
+        val image = File(filesDir, "camera_photos.png")
+        return FileProvider.getUriForFile(
+            this,
+            "com.example.cameratask.MainActivity",
+            image
+        )
+    }
+
+    private fun openCamera() {
+        binding.imageView.setImageURI(null)
+        contract.launch(imageURL)
+    }
+
+    private fun sharePicture() {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "image/jpeg"
+            putExtra(Intent.EXTRA_SUBJECT, "Bahno - android home task")
+            putExtra(Intent.EXTRA_TEXT, "link to github")
+            putExtra(Intent.EXTRA_STREAM, imageURL)
+        }
+        startActivity(intent)
+    }
+}
